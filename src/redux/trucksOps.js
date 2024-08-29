9d49037ca38a2fd820d9d7980c2e26968291f616
Angelina <midnightshout03@gmail.com>
2024-08-29 11:03:55 +0300

    Fix mistakes in code trucksOps

--- src/redux/trucksOps.js
+++ src/redux/trucksOps.js
@@ -7,7 +7,19 @@ export const fetchTrucks = createAsyncThunk(
     "trucks/fetchAll",
     async (_, thunkAPI) => {
         try {
-            const response = await axios.get("/campers");
+            const response = await axios.get("/");
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+export const fetchTrucksId = createAsyncThunk(
+    "trucks/fetchId",
+    async ({id}, thunkAPI) => {
+        try {
+            const response = await axios.get(`/campers/${id}`);
             return response.data;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);

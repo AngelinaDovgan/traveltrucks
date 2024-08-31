2dcfcedd6f6d6fe7ea2ec50b9b50df449fc6dea2
Angelina <midnightshout03@gmail.com>
2024-08-31 18:40:52 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,14 @@
-
+import css from './HomePage.module.css';
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    
+    return (
+        <div className={css.background}>
+            <div className={css.titlecont}>
+             <h1 className={css.ti}>Campers of your dreams</h1>
+             <h2 className={css.titl}>You can find everything you want in our catalog</h2>    
+            </div>
+       <Link className={css.button} to={"/catalog/"}>View now </Link>
+    </div>
+)
 }
